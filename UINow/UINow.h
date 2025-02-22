7aa437f3298693e53770af4b5b8b1c1398ee188e
Shivam Patel <31154386+shivampatel066@users.noreply.github.com>
2025-02-22 19:06:17 +0530

    Initial Commit

---
+++ UINow/UINow.h
@@ -0,0 +1,18 @@
+//
+//  UINow.h
+//  UINow
+//
+//  Created by Shivam Patel on 22/02/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UINow.
+FOUNDATION_EXPORT double UINowVersionNumber;
+
+//! Project version string for UINow.
+FOUNDATION_EXPORT const unsigned char UINowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UINow/PublicHeader.h>
+
+
---
+++ UINowTests/UINowTests.swift
@@ -0,0 +1,17 @@
+//
+//  UINowTests.swift
+//  UINowTests
+//
+//  Created by Shivam Patel on 22/02/25.
+//
+
+import Testing
+@testable import UINow
+
+struct UINowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
